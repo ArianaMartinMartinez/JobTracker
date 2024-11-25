277eae80abc5a499fad1da66f30aaaacfa95587e
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-25 11:27:52 +0100

    build: offer web and api routes done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\OfferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/offers', [OfferController::class, 'index'])->name('apiHomeOffers');
+Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apiShowOffer');
+Route::post('/offers', [OfferController::class, 'store'])->name('apiStoreOffer');
+Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiUpdateOffer');
+Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apiDestroyOffer');
