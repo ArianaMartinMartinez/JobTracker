2a89fe5d03873b2562d13ed2c54f1af10fb5b17e
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-28 12:33:17 +0100

    test: all OfferController functions tested

--- tests/Feature/OfferTest.php
+++ tests/Feature/OfferTest.php
@@ -2,19 +2,40 @@
 
 namespace Tests\Feature;
 
+use App\Models\Offer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class OfferTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_CheckIfCanReceiveAllOffersInJsonFile() {
+       $this->withoutExceptionHandling();
+
+       Offer::all();
+
+       $response = $this->get('/');
+
+       $response->assertStatus(200)
+            ->assertViewIs('home');
+    }
+
+    public function test_CheckIfCanReceiveOnOfferInJsonFile() {
+        $this->withoutExceptionHandling();
+
+        $offer = Offer::factory()->create([
+            'title' => 'Título de ejemplo',
+            'company' => 'Compañía de ejemplo',
+            'url' => 'https://github.com/ArianaMartinMartinez',
+            'status' => 'In progress',
+        ]);
+
+        $response = $this->get('/offers/' . $offer->id);
+
+        $response->assertStatus(200)
+            ->assertViewIs('show')
+            ->assertViewHas('offer', $offer);
     }
 }
