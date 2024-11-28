659eeb799a81cacf333ab93bcda368617e7d36cb
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-28 15:20:29 +0100

    test: all ProgressController functions tested

--- tests/Feature/Api/ProgressTest.php
+++ tests/Feature/Api/ProgressTest.php
@@ -2,19 +2,85 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Offer;
+use App\Models\Progress;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProgressTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_CheckIfCanReceiveAllProgressInJsonFile() {
+        $offer = Offer::factory(10)->create();
+        $progress = Progress::factory(5)->create();
+        $response = $this->get(route('apiHomeProgresses'));
+
+        $response->assertStatus(200)
+            ->assertJsonCount(5);
+    }
+
+    public function test_CheckIfCanReceiveOneProgressInJsonFile() {
+        $offer = Offer::factory(1)->create();
+        $progress = $this->post(route('apiStoreProgress'),[
+            'comment' => 'Esto es un progreso de ejemplo',
+            'id_offer' => 1,
+        ]);
+
+        $data = ['comment' => 'Esto es un progreso de ejemplo'];
+
+        $response = $this->get(route('apiShowProgress', 1));
+        $response->assertStatus(200)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_ChecKIfCanCreateNewProgressWithApi() {
+        $offer = Offer::factory(1)->create();
+        $response = $this->post(route('apiStoreProgress'), [
+            'comment' => 'Esto es un progreso de ejemplo',
+            'id_offer' => 1,
+        ]);
+
+        $response = $this->get(route('apiHomeProgresses'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1);
+    }
+
+    public function test_CheckIfCanUpdateProgressWithApi() {
+        $offer = Offer::factory(1)->create();
+        $response = $this->post(route('apiStoreProgress', [
+            'comment' => 'Esto es un progreso de ejemplo',
+            'id_offer' => 1,
+        ]));
+        $data = ['comment' => 'Esto es un progreso de ejemplo'];
+
+        $response = $this->get(route('apiHomeProgresses'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonFragment($data);
+
+        $response = $this->put(route('apiUpdateProgress', 1), [
+            'comment' => 'Esto es un progreso de ejemplo modificado',
+            'id_offer' => 1,
+        ]);
+        $data = ['comment' => 'Esto es un progreso de ejemplo modificado'];
+
+        $response = $this->get(route('apiHomeProgresses'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1)
+            ->assertJsonFragment($data);
+    }
+
+    public function test_CheckIfCanDeleteProgressWithApi() {
+        $offer = Offer::factory(1)->create();
+        $progress = Progress::factory(2)->create();
+
+        $response = $this->delete(route('apiDestroyProgress', 1));
+        $this->assertDatabaseCount('progress', 1);
+
+        $response = $this->get(route('apiHomeProgresses'));
+        $response->assertStatus(200)
+            ->assertJsonCount(1);
     }
 }
